7d40e24b99bca437e85865767f9d8419870751f5
Artem Zhuk <90647981+artmzhuk@users.noreply.github.com>
2023-10-01 23:27:34 +0300

    added one-element check Lab2.asm

--- Lab2.asm
+++ Lab2.asm
@@ -1,13 +1,14 @@
 assume CS:code,DS:data
  
 data segment
-a dw 10, 2, 21, 40, 45, 15, 8
+a dw 10
 len EQU $-a
 minDiff dw 65535
 minDiffIndex dw 0
-result10 db ?, ?, ?, ?, ?, 13, 10, '$'
+result10 db 5 dup (?), 13, 10, '$'
 diffStr db "Minimal diference is: $"
 indexStr db "Index: $"
+oneElementStr db "Array has only one element$"
 data ends
  
 ; Определить, какие два последовательных элемента массива наименее
@@ -41,6 +42,16 @@ mov cx, len
 mov si, len
 shr cx, 1
 sub cx, 1
+
+cmp cx, 0; checks array with len == 1
+jne moreThanOneElement
+mov dx, offset oneElementStr
+mov ah, 09h
+int 21h
+mov AX,4C00h
+int 21h
+moreThanOneElement:
+
 l1: ; for(int i = a.size - 1; i > 0; i++)
 	sub si, 2
 	mov ax, a[si]
@@ -58,8 +69,8 @@ l1: ; for(int i = a.size - 1; i > 0; i++)
 		sub bx, ax
 		cmp minDiff, bx ; if(minDiff > a[i-1] - a[i]
 		jbe endOfLoop
-			mov minDiff, cx
-			mov minDiffIndex, ax
+			mov minDiff, bx
+			mov minDiffIndex, cx
 			dec minDiffIndex
 		endOfLoop:
 loop l1
